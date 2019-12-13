3e1b5584b01899615e23f655ecaee9edee001def
evgenynovak <evnovak171@mail.ru>
2019-12-13 17:28:48 +0300

    bookOrganizer-app ver 0.1

---
+++ bookOrganizer-app/precache-manifest.c7e122ee058d2aa9033fad4e65eeeff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630ae08fad1bf11223c92d91fd669ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa49586d1f99b1788b46",
+    "url": "/static/css/main.ed908f43.chunk.css"
+  },
+  {
+    "revision": "cdbc1b86953afd3e655b",
+    "url": "/static/js/2.31f5d458.chunk.js"
+  },
+  {
+    "revision": "fa49586d1f99b1788b46",
+    "url": "/static/js/main.efa5a942.chunk.js"
+  },
+  {
+    "revision": "45a46612b3335912e3b0",
+    "url": "/static/js/runtime-main.240dbb06.js"
+  }
+]);
